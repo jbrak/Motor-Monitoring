b4e7972d3d4ff7bdb425edd94c1c15361702a0ff
Jbrak <joe@brakoniecki.com>
2019-11-18 19:45:11 +0000

    Put in a function

--- gauge.py
+++ gauge.py
@@ -1,26 +1,25 @@
 import plotly.graph_objects as go
-from random import randrange
-
-value = randrange(61)
 
 def bar_color(v):
     if v <= 10 : return 'red'
     elif  10 < v <= 20 : return 'yellow'
     elif  20 < v <= 60 : return 'green'
 
-fig = go.Figure(go.Indicator(
-    mode = "gauge+number+delta",
-    value = value,
-    delta = {'reference' : 40},
-    domain = {'x': [0, 1], 'y': [0, 1]},
-    gauge = {
-    'axis' : {'range' : [None, 60]},
-    'bar' :  {'color' : bar_color(value)},
-    'steps' : [
-            {'range': [0, 10], 'color': 'lightgrey'},
-            {'range': [10,20], 'color': 'darkgrey'},
-            {'range': [20,60], 'color': 'grey'}]
-            }
-))
+def plot(val):
+    fig = go.Figure(go.Indicator(
+        mode = "gauge+number+delta",
+        value = val,
+        delta = {'reference' : 40},
+        domain = {'x': [0, 1], 'y': [0, 1]},
+        gauge = {
+        'axis' : {'range' : [None, 60]},
+        'bar' :  {'color' : bar_color(val)},
+        'steps' : [
+                {'range': [0, 10], 'color': 'lightgrey'},
+                {'range': [10,20], 'color': 'darkgrey'},
+                {'range': [20,60], 'color': 'grey'}]
+                }
+    ))
+    fig.write_html('gauge.html', auto_open=True)
 
-fig.write_html('gauge.html', auto_open=True)
+plot(10)
